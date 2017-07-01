ef771cefdf0e2caceb3cc1af2d61b61955de5754
panghongye <cc_@live.com>
2017-07-01 18:26:11 +0800

    add game

--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,6 @@ export default () => (
     <li><Link href='/blog?id=first' as='/blog/first'><a>My first blog post</a></Link></li>
     <li><Link href='/blog?id=second' as='/blog/second'><a>My second blog post</a></Link></li>
     <li><Link href='/blog?id=last' as='/blog/last'><a>My last blog post</a></Link></li>
+    <li><Link href='/game' as='/game'><a>game</a></Link></li>
   </ul>
 )
