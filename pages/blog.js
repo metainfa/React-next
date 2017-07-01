254c9687821f92827524c4f235820e47349289f6
panghongye <cc_@live.com>
2017-07-01 18:51:24 +0800

    0

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
 export default class extends React.Component {
-  static getInitialProps ({ query: { id } }) {
+  static getInitialProps({ query: { id } }) {
     return { id }
   }
 
-  render () {
-    return <div>
-      <h1>My {this.props.id} blog post</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-        saldkjflksajdflkjasdlfkjlksadf
-      </p>
-    </div>
+  render() {
+
+    var ele = [];
+    for (let i = 0; i < 4; i++) {
+      ele.push(<p style={{background:'red'}}>{i}</p>)
+    }
+
+
+    return (
+      <div>
+        <h1>My {this.props.id} blog post</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+
+        {ele}
+
+      </div>
+    )
   }
 }
